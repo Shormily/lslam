f7085d5c2ed452aeb44378c9af7eb28f4a35d06d
Shormily <shormilyrais@gmail.com>
2025-03-20 13:37:02 +0600

    ok

---
+++ src/app/Component/Navbar/shared/Footer/FirstPage/page.tsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+    <>
+     
+          <p>
+          hi its 
+          </p>
+          
+      
+     
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
 import Navbar from "./Component/Navbar/Navbar";
 import Footer from "./Component/Navbar/shared/Footer/page";
+import FirstPage from "./Component/Navbar/shared/Footer/FirstPage/page";
+import { Crete_Round } from "next/font/google";
+
+const creteRound = Crete_Round({
+  subsets: ["latin"],
+  weight: ["400"], // Crete Round only has a weight of 400
+  variable: "--font-crete-round",
+});
 
 export default function Home() {
   return (
     <>
+      <div className={`${creteRound.variable} font-serif`}>
       <Navbar />
-      ok
+     <FirstPage/>
       <Footer/>
+       </div>
+     
     </>
   );
 }
