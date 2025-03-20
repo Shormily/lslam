4f8a86cfdc9d6171574d08dd0141e624c8dba730
Shormily <shormilyrais@gmail.com>
2025-03-20 19:41:50 +0600

    ok done

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,4 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/dashboard","/event"] }
+export const config = { matcher: ["/dashboard"] }
+// you can create all private routes
