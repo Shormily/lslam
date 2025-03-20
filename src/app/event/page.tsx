149ff51afbece3f732e17afee2a23f0ca0832feb
Shormily <shormilyrais@gmail.com>
2025-03-20 15:44:19 +0600

    all ok

--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Slide from '../Component/Navbar/shared/Footer/Slider/page';
 import EventBanner from '../Component/Navbar/shared/Footer/EventBanner/EventBanner';
 import Events from '../Component/Navbar/shared/Footer/Event/Event';
+import FivePillars from '../Component/Navbar/shared/Footer/Five/page';
 
 const page = () => {
     return (
         <>
             <Slide />
             <EventBanner />
-            <Events/>
+            <Events />
+            <FivePillars />
+            
         </>
     );
 };
